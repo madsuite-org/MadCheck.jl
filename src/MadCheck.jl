a31e7715cf3b173aa2fe7b1bbb4915701f838cdc
GhislainDv <ghislain.dv410@gmail.com>
2026-09-08 11:35:11 +0200

    SOSC_check and small corrections

--- src/MadCheck.jl
+++ src/MadCheck.jl
@@ -11,6 +11,7 @@ include("active_set.jl")
 include("jacobian_degeneracy.jl")
 include("MFCQ_direction_condition.jl")
 include("constraint_qualification.jl")
+include("SOSC.jl")
 include("Models/feas_l1_model.jl")
 include("feasibility.jl")
 
---
+++ src/SOSC.jl
@@ -0,0 +1,47 @@
+"""
+    check_SOSC(nlp, results, active_method, tol)
+
+Compute the reduced hessian of the KKT system to check if it is positive definite and thus check if SOSC is verified.
+    
+Return
+If the reduced hessian is empty, returns `Inf`, else returns the value of the smallest eigenvalue of the reduced hessian.
+"""
+function check_SOSC(nlp, results, active_method::AbstractActiveSetMethod, tol::Float64)
+    # Jacobian
+    active, active_boundary = find_active(nlp, results, active_method)
+
+    Jac, indices_to_constraints = build_work_jacobian(nlp, results, active, active_boundary)
+    n_jac, n = size(Jac)
+
+    # Checks
+    SCS = check_SCS(nlp, results, tol)
+    if length(SCS.bounds) + length(SCS.constraints) != 0
+        @warn "Strict complementarity is not verified"
+    end
+
+    QR = MadCheck.find_degenerate(Jac, DegenJacQR(tol))
+    if length(QR)!=0 && length(QR[1]) != 0
+        @warn "Jacobian is not full row rank"
+    end
+
+    # Hessian
+    x = results.solution
+    y = results.multipliers
+    Wi, Wj = NLPModels.hess_structure(nlp)
+    Wx = NLPModels.hess_coord(nlp, x, y)
+    Hess = sparse(Wi, Wj, Wx, n, n)
+
+    # Saddle point matrix
+    K = Array([Hess Jac'; Jac zeros((n_jac, n_jac))])
+
+    Z = LinearAlgebra.nullspace(K)
+
+    # Reduced Hessian
+    H = Z' * K * Z
+
+    if isempty(H)
+        return Inf
+    end
+
+    return eigmin(Symmetric(H))
+end
